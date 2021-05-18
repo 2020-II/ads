f34f06819c2ddc59b7906d6ece9cf5b368110106
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2021-05-17 22:05:38 -0500

    Sesion02

---
+++ ADSE2InicioMaven/src/main/java/pe/unjfsc/ads/sesion02/view/CV01MostrarMensaje.java
@@ -0,0 +1,11 @@
+package pe.unjfsc.ads.sesion02.view;
+
+public class CV01MostrarMensaje {
+
+    public static void main(String[] args) {
+        String sMensaje = "Iniciando Maven with Java";
+        
+        System.out.println(sMensaje);
+    }
+    
+}
